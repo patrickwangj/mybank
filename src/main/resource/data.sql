44e6669552326159859e0fdbe27daae2d903358a
patrickwangj <280830599@qq.com>
2017-01-12 10:08:20 +0800

    Add files via upload

---
+++ src/main/resource/data.sql
@@ -0,0 +1,11 @@
+create table office (id int(2) primary key auto_increment, country char(25), city char(25), contact char(25) );
+
+insert into office (country, city, contact) values ('China','Shenzhen','cn@wbank.com');
+insert into office (country, city, contact) values ('India','Armrisar','in@wbank.com');
+insert into office (country, city, contact) values ('Malaysia','Pengang','my@wbank.com');
+insert into office (country, city, contact) values ('Sir Lanka','Galle','lk@wbank.com');
+insert into office (country, city, contact) values ('Japan','Matsusaka','jp@wbank.com');
+insert into office (country, city, contact) values ('Austria','Salzburg','at@wbank.com');
+insert into office (country, city, contact) values ('Belgium','Antpur','be@wbank.com');
+
+commit;
